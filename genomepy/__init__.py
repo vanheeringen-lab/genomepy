e42f4b0b2fcec8ab52d90095dd0846db7e0bd216
Simon van Heeringen <simon.vanheeringen@gmail.com>
2017-01-26 08:30:12 +0100

    fix genomepy providers command

--- genomepy/__init__.py
+++ genomepy/__init__.py
@@ -3,6 +3,7 @@ import genomepy.functions
 __version__ = '0.2.0'
 __author__ = "Simon van Heeringen"
 
+list_available_providers = genomepy.functions.list_available_providers
 list_available_genomes = genomepy.functions.list_available_genomes
 list_installed_genomes = genomepy.functions.list_installed_genomes
 search = genomepy.functions.search
