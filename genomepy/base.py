c0af0671431f57617c5f241fa2b853af81236857
Simon van Heeringen <simon.vanheeringen@gmail.com>
2017-10-21 20:42:53 +0200

    docstrings

--- genomepy/base.py
+++ genomepy/base.py
@@ -7,6 +7,8 @@ import norns
 config = norns.config("genomepy", default="cfg/default.yaml")
 
 class Plugin(object):
+    """Plugin base class.
+    """
     active = False
 
     def name(self):
@@ -25,8 +27,9 @@ class Plugin(object):
     def get_properties(self, genome):
         raise NotImplementedError("plugin should implement this method")
 
-
 def find_plugins():
+    """Locate and initialize all available plugins.
+    """ 
     plugin_dir = os.path.dirname(os.path.realpath(__file__))
     plugin_dir = os.path.join(plugin_dir, "plugins")
     plugin_files = [x[:-3] for x in os.listdir(plugin_dir) if x.endswith(".py")]
@@ -51,6 +54,13 @@ def convert(name):
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
 def init_plugins():
+    """Return dictionary of available plugins
+
+    Returns
+    -------
+    plugins : dictionary
+        key is plugin name, value Plugin object
+    """ 
     find_plugins()
     d = {}
     for c in Plugin.__subclasses__():
@@ -64,21 +74,34 @@ def init_plugins():
     return d
 
 def activate(name):
+    """Activate plugin.
+
+    Parameters
+    ----------
+    name : str
+        Plugin name.
+    """
     if name in plugins:
         plugins[name].activate()
     else:
         raise Exception("plugin {} not found".format(name))
 
 def deactivate(name):
+    """Deactivate plugin.
+
+    Parameters
+    ----------
+    name : str
+        Plugin name.
+    """
     if name in plugins:
         plugins[name].deactivate()
     else:
         raise Exception("plugin {} not found".format(name))
 
 def get_active_plugins():
+    """Returns all active plugin instances.
+    """ 
     return [inst for name, inst in plugins.items() if inst.active]
 
 plugins = init_plugins()
-
-
-
