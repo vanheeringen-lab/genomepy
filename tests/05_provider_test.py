35f74a89e0ac6ce426f6a0908624cc7573701dba
siebrenf <48289046+siebrenf@users.noreply.github.com>
2020-05-27 22:58:57 +0200

    hotfixes 2: electric boogaloo (#106)
    * another workaround for #102: download gtf files directly where possible. this fixes the issue more most genomes
    
    * fixed test
    
    * fixed #106: text format annotations files will be checked for wrong fields, which are replaced with zeroes.
    
    * cc
    
    * cc

--- tests/05_provider_test.py
+++ tests/05_provider_test.py
@@ -191,10 +191,10 @@ def test_download_annotation(p):
     localname = "my_annot"
 
     p = p.create("UCSC")
-    name = "sacCer3"
+    name = "xenTro2"
 
     annot_url = (
-        "http://hgdownload.cse.ucsc.edu/goldenPath/sacCer3/database/ensGene.txt.gz"
+        "http://hgdownload.cse.ucsc.edu/goldenPath/xenTro2/database/ensGene.txt.gz"
     )
     with TemporaryDirectory(dir=out_dir) as tmpdir:
         p.download_annotation(name=name, genomes_dir=tmpdir, localname=localname)
