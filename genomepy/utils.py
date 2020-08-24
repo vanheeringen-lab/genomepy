3de64fcf91ba17d3f7bafd6a0cebb638c1f63770
siebrenf <siebrenf@gmail.com>
2020-08-24 12:06:11 +0200

    check_url retry time randomized

--- genomepy/utils.py
+++ genomepy/utils.py
@@ -13,6 +13,7 @@ import time
 from glob import glob
 from norns import exceptions
 from pyfaidx import Fasta
+from random import randint
 from socket import timeout
 from tempfile import TemporaryDirectory
 
@@ -347,7 +348,7 @@ def check_url(url, time_out=3, max_tries=1, _try=1):
     except (urllib.request.URLError, timeout):
         # Some providers get swamped with requests, so we allow retries
         if _try <= max_tries:
-            time.sleep(1)
+            time.sleep(randint(3,10)*0.1)  # sleep between 0.3 and 1 second
             return check_url(url, time_out=time_out, max_tries=max_tries, _try=_try + 1)
         return False
 
