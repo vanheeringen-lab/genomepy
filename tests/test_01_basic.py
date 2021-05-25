c2d95df1bb9c25862a4801280c1c594444d402a9
siebrenf <siebrenf@gmail.com>
2021-05-25 17:31:37 +0200

    improved black & flake8 feedback

--- tests/test_01_basic.py
+++ tests/test_01_basic.py
@@ -11,15 +11,37 @@ import genomepy
 from . import travis
 
 
-@pytest.mark.skipif(travis, reason="format before committing!")
 def test_black_formatting():
-    sp.check_call("black setup.py genomepy/ tests/", shell=True)
+    if not travis:
+        sp.check_call("black setup.py genomepy/ tests/", shell=True)
+        pass
+
+    try:
+        sp.check_output(
+            "black --check setup.py genomepy/ tests/", stderr=sp.STDOUT, shell=True
+        )
+    except sp.CalledProcessError as e:
+        msg = e.output.decode("utf-8")
+        msg = "\n".join(msg.split("\n")[:-3])  # multi line error
+        print("Black output:")
+        print(msg)
+
+        msg = ", ".join(msg.split("\n"))[:-2]  # single line error
+        pytest.fail(msg)
 
 
-@pytest.mark.skipif(travis, reason="format before committing!")
 def test_flake8_formatting():
-    ret = sp.check_call("flake8 setup.py genomepy/ tests/", shell=True)
-    assert ret == 0
+    try:
+        sp.check_output(
+            "flake8 setup.py genomepy/ tests/", stderr=sp.STDOUT, shell=True
+        )
+    except sp.CalledProcessError as e:
+        msg = e.output.decode("utf-8")  # multi line error
+        print("Flake8 output:")
+        print(msg)
+
+        msg = ", ".join(msg.split("\n"))[:-2]  # single line error
+        pytest.fail(msg)
 
 
 @pytest.mark.skipif(not travis, reason="it works locally all right")
