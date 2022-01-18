1430196853179700e8a88435c00324deb4ec4713
Tilman Schäfers <t.schaefers@science.ru.nl>
2022-01-18 12:39:10 +0100

    removed cache imports/variables

--- genomepy/caching.py
+++ genomepy/caching.py
@@ -2,8 +2,6 @@ import os
 from shutil import rmtree
 
 from appdirs import user_cache_dir
-from bucketcache import Bucket
-from joblib import Memory
 from diskcache import Cache
 
 from genomepy.__about__ import __version__
@@ -15,14 +13,7 @@ cache_exp_long = 3600 * 24
 genomepy_cache_dir = os.path.join(user_cache_dir("genomepy"), __version__)
 os.makedirs(genomepy_cache_dir, exist_ok=True)
 
-# Store the output of slow commands (marked with @cache and @goldfish_cache) for fast reuse.
-# Bucketcache creates a new pickle for each function + set of unique variables,
-# For class methods, use ignore=["self"] or use @staticmethod.
-cache = Bucket(genomepy_cache_dir, days=7)
-goldfish_cache = Bucket(genomepy_cache_dir, minutes=10)
-
-memory = Memory(genomepy_cache_dir, verbose=0)
-
+# Store the output of slow commands (marked with @disk_cache.memoize) for fast reuse.
 disk_cache = Cache(directory=genomepy_cache_dir)
 
 
