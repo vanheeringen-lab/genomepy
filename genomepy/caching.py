b6f5c3834d0c082750ee43cc3293c69135d60ec0
Tilman Schäfers <t.schaefers@science.ru.nl>
2022-04-21 15:09:51 +0200

    set default cache limit

--- genomepy/caching.py
+++ genomepy/caching.py
@@ -11,7 +11,7 @@ from genomepy.config import config
 cache_exp_short = 3.6e3
 cache_exp_long = 8.64e4
 # Set max size for cache (in bytes) from config
-cache_size_limit = config.get("cache_size_limit")
+cache_size_limit = config.get("cache_size_limit", 600000000)
 
 genomepy_cache_dir = os.path.join(user_cache_dir("genomepy"), __version__)
 os.makedirs(genomepy_cache_dir, exist_ok=True)
