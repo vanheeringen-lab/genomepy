071b7e782983b91371ead6b54705ea352d373f73
Tilman Schäfers <t.schaefers@science.ru.nl>
2022-05-11 10:50:02 +0200

    comment

--- genomepy/caching.py
+++ genomepy/caching.py
@@ -17,7 +17,7 @@ genomepy_cache_dir = os.path.join(user_cache_dir("genomepy"), __version__)
 os.makedirs(genomepy_cache_dir, exist_ok=True)
 
 # Store the output of slow commands (marked with @disk_cache.memoize) for fast reuse
-# diskcache uses the LRU (least-recently-stored) eviction policy by default
+# DiskCache uses the LRU (least-recently-stored) eviction policy by default
 disk_cache = Cache(directory=genomepy_cache_dir, size_limit=int(cache_size_limit))
 
 
