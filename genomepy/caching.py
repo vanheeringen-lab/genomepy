a27a0097c692475c6a3ca8292d2cd73275d78f9a
Tilman Schäfers <t.schaefers@science.ru.nl>
2022-05-20 15:34:43 +0200

    removed cache size limit

--- genomepy/caching.py
+++ genomepy/caching.py
@@ -5,20 +5,18 @@ from appdirs import user_cache_dir
 from diskcache import Cache
 
 from genomepy.__about__ import __version__
-from genomepy.config import config
 
 # Set cache expiration times (in seconds)
 cache_exp_short = 3.6e3
 cache_exp_long = 8.64e4
-# Set max size for cache (in bytes) from yml config
-cache_size_limit = config.get("cache_size_limit", 600000000)
+
 
 genomepy_cache_dir = os.path.join(user_cache_dir("genomepy"), __version__)
 os.makedirs(genomepy_cache_dir, exist_ok=True)
 
 # Store the output of slow commands (marked with @disk_cache.memoize) for fast reuse
 # DiskCache uses the LRU (least-recently-stored) eviction policy by default
-disk_cache = Cache(directory=genomepy_cache_dir, size_limit=int(cache_size_limit))
+disk_cache = Cache(directory=genomepy_cache_dir)
 
 
 def clean():
