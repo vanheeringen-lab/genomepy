df35379ad4954ea889407b9ce6bffbc3dc3de6b3
siebrenf <siebrenf@gmail.com>
2022-08-01 15:27:34 +0200

    fix current publishing issues

--- genomepy/__about__.py
+++ genomepy/__about__.py
@@ -1,3 +1,5 @@
 """Metadata"""
 __version__ = "0.14.0"
-__author__ = "Siebren Frölich, Maarten van der Sande, Tilman Schäfers and Simon van Heeringen"
+__author__ = (
+    "Siebren Frölich, Maarten van der Sande, Tilman Schäfers and Simon van Heeringen"
+)
