80d7a4cd0196d7db847c64ccfd68091dcf02e095
siebrenf <siebrenf@gmail.com>
2025-09-30 11:41:35 +0200

    lint

--- genomepy/files.py
+++ genomepy/files.py
@@ -188,25 +188,25 @@ def extract_tarball(fname, outfile=None, concat=True) -> Union[str, None]:
     # Extract files to temporary directory
     tmp_dir = mkdtemp(dir=os.path.dirname(outfile))
     with tarfile.open(fname) as tar:
+
         def is_within_directory(directory, target):
-            
+
             abs_directory = os.path.abspath(directory)
             abs_target = os.path.abspath(target)
-        
+
             prefix = os.path.commonprefix([abs_directory, abs_target])
-            
+
             return prefix == abs_directory
-        
+
         def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
-        
+
             for member in tar.getmembers():
                 member_path = os.path.join(path, member.name)
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
-        
-            tar.extractall(path, members, numeric_owner=numeric_owner) 
-            
-        
+
+            tar.extractall(path, members, numeric_owner=numeric_owner)
+
         safe_extract(tar, path=tmp_dir)
     for root, _, files in os.walk(tmp_dir):
         fnames += [os.path.join(root, fname) for fname in files]
